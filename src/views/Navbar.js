d8c0096ac0b97498879f2dbe591b8ec7f9d5e57b
iamnamananand996 <iamnamananand996@gmail.com>
2020-12-23 21:15:47 +0530

    fix

--- src/views/Navbar.js
+++ src/views/Navbar.js
@@ -30,43 +30,43 @@ export default function Navbar() {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto ml-auto">
             <li className="nav-item active">
-              <a className="nav-link" href="1#" style={{ color: "#EF6666" }}>
+              <a className="nav-link" href="!#" style={{ color: "#EF6666" }}>
                 Home <span className="sr-only">(current)</span>
               </a>
             </li>
             <li className="nav-item active">
-              <a className="nav-link" href="1#">
+              <a className="nav-link" href="!#">
                 Consumer
               </a>
             </li>
             <li className="nav-item active">
-              <a className="nav-link" href="1#">
+              <a className="nav-link" href="!#">
                 Enterprise
               </a>
             </li>
             <li className="nav-item active">
-              <a className="nav-link" href="1#">
+              <a className="nav-link" href="!#">
                 Live Stream
               </a>
             </li>
             <li className="nav-item active">
-              <a className="nav-link" href="1#">
+              <a className="nav-link" href="!#">
                 Soprono U
               </a>
             </li>
             <li className="nav-item active">
-              <a className="nav-link" href="1#">
+              <a className="nav-link" href="!#">
                 Services
               </a>
             </li>
             <li className="nav-item active">
-              <a className="nav-link" href="1#">
+              <a className="nav-link" href="!#">
                 Sign Up
               </a>
             </li>
 
             {/* <li className="nav-item">
-              <a className="nav-link disabled" href="1#">
+              <a className="nav-link disabled" href="!#">
                 Disabled
               </a>
             </li> */}
